04956fc76e452d99995a6bc207f717151fec6681
jinc132 <jinc132@u.washington.edu>
2018-07-16 14:33:20 -0700

    finish css for cards

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import _ from 'lodash';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import './style.css';
 
 class App extends Component {
     constructor(props) {
