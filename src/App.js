57403a2c13ca0ce9a2352b49a4c2563ebe022d27
jinc132 <jinc132@u.washington.edu>
2018-07-16 13:10:45 -0700

    Add files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import {} from 'reactstrap';;
+import _ from 'lodash';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            movies: [],
             menu: false
         };
     }
@@ -20,32 +22,104 @@ class App extends Component {
         return (
             <div>
                 <header>
-                    <Menu toggle={() => this.toggleMenu()} />
+
                 </header>
+                <main>
+                    <div id="movieList" className="col-9">
+                        <MovieList movies= {this.state.movies} adoptCallback={(individualMovie) => this.selectedMovie(individualMovie)} />
+                        <Popup movie={this.state.singleMovie} toggle={() => this.toggle()} modal={this.state.modal} />
+                    </div>
+                </main>
+            </div>
+        );
+    }
+
+    toggle() {
+        let mode = this.state.modal;
+        this.setState({
+            modal: !mode
+        });
+    }
+
+    selectedMovie(movie) {
+        let selectedMovie = _.find(this.state.movies, { 'title': movie });
+        this.setState({ singleMovie: selectedMovie, modal: true });
+    }
+}
+
+class Popup extends Component {
+    render() {
+        let current = this.props.movie;
+        if (current === undefined) return null;
+
+        return (
+            <div>
+                <Modal className="modalPopup" isOpen={this.props.modal} fade={false} toggle={this.props.toggle}>
+                    <ModalHeader toggle={this.props.toggle}>{current.title}</ModalHeader>
+                    <ModalBody>
+                        <img src={'http://image.tmdb.org/t/p/w200//' + current.poster_path} alt={current.poster_path} />
+                        <div className="info">
+                            <h3>Release Date</h3>
+                            <p>{current.release_date}</p>
+                            <h3>Vote Average</h3>
+                            <p>{current.vote_average}</p>
+                        </div>
+                        <h3>Overview</h3>
+                        <p>{current.overview}</p>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="primary" onClick={() => this.props.toggle()}>Close</Button>
+                    </ModalFooter>
+                </Modal>
+            </div >
+        );
+    }
+}
+class MovieList extends Component {
+    render() {
+        let movieArray = this.props.movies.map((movieObj) => {
+            return <MovieCard key={movieObj.title} movieCard={movieObj} adoptCallback={this.props.adoptCallback} />;
+        });
+        return (
+            <div>
+                <div className="movie-deck">
+                    {movieArray}
+                </div>
             </div>
         );
     }
 }
 
-class Menu extends Component {
+class MovieCard extends Component {
     render() {
+        let movieCard = this.props.movieCard;
         return (
-            <div>
-                <Navbar>
-                    <Navbar.Collapse>
-                        <Nav>
-                            <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-                                <MenuItem eventKey={3.1}>Action</MenuItem>
-                                <MenuItem eventKey={3.2}>Another action</MenuItem>
-                                <MenuItem eventKey={3.3}>Something else here</MenuItem>
-                                <MenuItem divider />
-                                <MenuItem eventKey={3.3}>Separated link</MenuItem>
-                            </NavDropdown>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Navbar>
-            </div >
+            <div className="card">
+                <img className="card-img-top" src={'http://image.tmdb.org/t/p/w200//' + movieCard.poster_path} alt={movieCard.title} onClick={(e) => this.props.adoptCallback(movieCard.title)} />
+                <div className="card-body">
+                    <h3 className="card-title">{movieCard.title}</h3>
+                    <p className="card-date">{movieCard.release_date}</p>
+                </div>
+            </div>
         );
     }
 }
+
+class CheckBoxes extends Component {
+
+    handleClick(maxValue) {
+        this.props.toggleCallback(maxValue)
+    }
+
+    render() {
+        return (
+            <ol className="checkbox">
+                <li onClick={() => this.handleClick(7)}>MUST SEE!! (Rate > 7)</li>
+                <li onClick={() => this.handleClick(5)}>Movies to kill time (Rate > 5)</li>
+                <li onClick={() => this.handleClick(0)}>See all the movies</li>
+            </ol >
+        );
+    }
+}
+
 export default App;
