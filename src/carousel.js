f877abcb13c07c5fecd132e67abe730924d40730
justin38 <justinya07@gmail.com>
2018-07-18 13:59:20 -0700

    basket

--- src/carousel.js
+++ src/carousel.js
@@ -1,43 +1,88 @@
 import './style.css';
 import React, { Component } from "react";
-import Slider from "../src/slider";
+import Slider from "react-slick";
+import firebase from 'firebase/app';
 
-export default class MultipleItems extends Component {
+export class Carousel extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: [],
+      basketRef: undefined
+    };
+  }
+
+  componentDidMount() {
+    let basketRef = firebase.database().ref('baskets').child(this.props.user.uid);
+    basketRef.on('value', (snapshot) => {
+      let snap = snapshot.val();
+      let keys = Object.keys(snap);
+      let array = keys.map((key) => {
+        snap[key].key = key;
+        return snap[key]
+      });
+      if (array) {
+        this.setState({
+          movies: array,
+          basketRef: basketRef
+        })
+      } else {
+        return null;
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    if (this.state.basketRef) {
+      this.state.basketRef.off();
+    }
+  }
+
   render() {
     const settings = {
       dots: true,
       infinite: true,
       speed: 500,
-      fade: true,
-      slidesToShow: 3,
-      slidesToScroll: 3
+      slidesToShow: 2,
+      slidesToScroll: 2
     };
+    // let keys = Object.keys(this.state.movies);
+    let cards = this.state.movies.map((key) => {
+      return <Card movieCard={key} />
+    })
     return (
       <div>
-        <h2> Multiple items </h2>
+        <h2> My Basket </h2>
         <Slider {...settings}>
-          <div>
-            <h3>1</h3>
-          </div>
-          <div>
-            <h3>2</h3>
-          </div>
-          <div>
-            <h3>3</h3>
-          </div>
-          <div>
-            <h3>4</h3>
-          </div>
-          <div>
-            <h3>5</h3>
-          </div>
-          <div>
-            <h3>6</h3>
-          </div>
+
+            {cards}
+
         </Slider>
       </div>
     );
   }
 }
+
+class Card extends Component {
+  render() {
+    let movieCard = this.props.movieCard;
+    return (
+      <div>
+        <div className="card">
+          <img className="card-img-top" src={'http://image.tmdb.org/t/p/w185//' + movieCard.poster_path} alt={movieCard.title} />
+          <div className="card-body">
+            <h3 className="card-title">{movieCard.title}</h3>
+            <p className="card-date">{movieCard.release_date}</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+
+}
+
+
 
 export default Carousel
